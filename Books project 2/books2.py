1cecdaa9c81276e9ce8953392c506df3917f159a
Prajwal Reddy <reddyprajwal2000@gmail.com>
2026-02-04 11:56:34 +0100

    adding new field & pydantic validation

--- Books project 2/books2.py
+++ Books project 2/books2.py
@@ -12,13 +12,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
     
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
         
 class BookRequest(BaseModel):
     id: Optional[int] = Field(description = "ID is not needed on create", default = None)
@@ -26,6 +28,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length = 1)
     description: str = Field(min_length = 1, max_length = 10000)
     rating: int = Field(gt=0, lt=5)
+    published_date: int = Field(gt = 1995, lt = 2020)
     
     
     model_config = {
@@ -34,7 +37,8 @@ class BookRequest(BaseModel):
                 "title": "New Book",
                 "author": "Prajwal Reddy",
                 "description": "History book for children",
-                "rating": 4
+                "rating": 4,
+                "published_date": 2020
             }
         }
     }
@@ -43,12 +47,12 @@ class BookRequest(BaseModel):
 
 
 BOOKS = [
-    Book(1,"Computer Science Noob", "Prajwal", "Great book", 10),
-    Book(2,"Social Science Noob", "Prajwal Reddy", "A very Great book", 8),
-    Book(3,"History Noob", "Alice", "Just a fine book", 7),
-    Book(4,"Biology Noob", "Bob", "A very very Great book", 9),
-    Book(5,"Chemistry Noob", "Nathan", "Decent book", 5),
-    Book(6,"Fifty shades of coding", "Reddy", "A veryyyyyy Great book", 9)
+    Book(1,"Computer Science Noob", "Prajwal", "Great book", 10, 2012),
+    Book(2,"Social Science Noob", "Prajwal Reddy", "A very Great book", 8, 2013),
+    Book(3,"History Noob", "Alice", "Just a fine book", 7, 2012),
+    Book(4,"Biology Noob", "Bob", "A very very Great book", 9, 2016),
+    Book(5,"Chemistry Noob", "Nathan", "Decent book", 5, 2017),
+    Book(6,"Fifty shades of coding", "Reddy", "A veryyyyyy Great book", 9, 2015)
 ]
 
 @app.get("/books")
@@ -67,6 +71,51 @@ def create_book(book_request:BookRequest):
     BOOKS.append(find_book_id(new_book))
     
     
+#endpoint to fetch the book by ID  
+@app.get("/book/{book_id}")
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+#endppoint to fetch the book by published_date
+@app.get("/books/{published_date}")
+async def by_published_date(published_date: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+    return books_to_return
+
+
+    
+#endpoint to fetch book by rating
+@app.get("/books/{rating}")
+async def book_rating(rating: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
+#PUT METHODS
+@app.put("/books/update_book")
+async def update_book(book: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+            
+
+#DELETE METHODS
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
+        
+
 
 def find_book_id(book: Book):
     if len(BOOKS) > 0:
