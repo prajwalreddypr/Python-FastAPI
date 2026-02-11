810c02134e31ced00e1b0717562b0f65f697bd05
Prajwal Reddy <reddyprajwal2000@gmail.com>
2026-02-11 12:26:59 +0100

    put and post requests

--- Project 3/main.py
+++ Project 3/main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI, HTTPException, Path
+from pydantic import BaseModel, Field
 from starlette import status
 import models
 from database import SessionLocal, engine
@@ -20,12 +21,22 @@ def get_db():
         
     
 db_dependency = Annotated[Session, Depends(get_db)]
-    
+
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length = 100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+
+
+
             
 @app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
-        
+
 @app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
 async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
@@ -33,3 +44,29 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
         return todo_model
     
     raise HTTPException(status_code=404, detail = 'Todo not found')
+
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.dict())
+    
+    db.add(todo_model)
+    db.commit()
+    
+    
+@app.put("/todo/{todo_id}", status_code = status_code.HTTP_204_NO_CONTENT )
+async def update_todo(db: db_dependency,todo_id: int, todo_request: TodoRequest
+):
+
+todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+if todo_model is None:
+    raise HTTPException(status_code=404, detail = 'Todo not found')
+
+todo_model.title = todo_request.title
+todo_model.description = todo_request.description
+todo_model.priority = todo_request.priority
+todo_model.complete = todo_request.complete
+
+db.add(todo_model)
+db.commit()
