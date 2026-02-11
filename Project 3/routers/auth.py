d08c187ef0bb8805197effbe919fce99a254937a
Prajwal Reddy <reddyprajwal2000@gmail.com>
2026-02-11 15:39:14 +0100

    authentication endpoints

--- Project 3/routers/auth.py
+++ Project 3/routers/auth.py
@@ -6,6 +6,10 @@ from database import SessionLocal
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
+
+
+
 
 router = APIRouter()
 
@@ -32,6 +36,13 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
 
     
 
@@ -55,3 +66,12 @@ async def create_user(db: db_dependency,
     db.commit() 
 
 
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Failed Authentication'
+    
+
+    return 'Authentication Successfull'
