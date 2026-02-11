f7b2e40618bb94c37eedff9b9c140ebe63c88852
Prajwal Reddy <reddyprajwal2000@gmail.com>
2026-02-11 15:24:57 +0100

    authentication

--- Project 3/models.py
+++ Project 3/models.py
@@ -1,5 +1,24 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String) #encrypted pwd
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+
+
+
+
+
 
 
 class Todos(Base):
@@ -10,6 +29,7 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default = False)
+    owner_id = Column(Integer, ForeignKey("user.id"))
     
     
     
